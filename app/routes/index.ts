c564ba786d6d6ce1d74a24e9d4019b68d095dccd
Ushinji <siguma971@gmail.com>
2019-08-05 14:26:24 +0900

    Display images from `users/self/media/recent/`  api

--- app/routes/index.ts
+++ app/routes/index.ts
@@ -1,9 +1,23 @@
 import { Router, Response } from 'express';
+import axios from 'axios';
+const ACCESS_TOKEN = process.env.INSTAGRAM_API_ACCESS_TOKEN;
 
 const routes = Router();
 
-routes.get('/', (_, res: Response) => {
-  res.render('./index.ejs');
+routes.get('/', async (_, res: Response) => {
+  const axiosData = await axios.get(
+    'https://api.instagram.com/v1/users/self/media/recent/',
+    {
+      params: {
+        access_token: ACCESS_TOKEN,
+      },
+    }
+  );
+  const data = axiosData.data;
+  const imageUrls = data.data.map((element: any) => {
+    return element.images.low_resolution.url;
+  });
+  res.render('./index.ejs', { imageUrls: imageUrls });
 });
 
 routes.get(
